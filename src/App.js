a5444208deeb0f0604a7beecd4d291769eb04882
Issac Moreno <issaxavier909@gmail.com>
2020-02-03 13:04:39 -0800

    Added reducers folder, actions folder, utils folder and middlware

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { reducer } from '../reducers'
+import { createStore, applyMiddlware } from 'redux';
+import { Provider } from 'react-redux'
+// import thunk from 'redux-thunk';
+// import logger from 'redux-logger';
+
+import Chart from './components/Chart';
+
 import './App.css';
+
+const store = createStore(
+  reducer, 
+  // applyMiddleware (thunk, logger)
+)
 
 function App() {
   return (
-    <div className="App">
-   
-    </div>
+    <Provider store = {store}>
+      <div className="App">
+        <Chart />
+      </div>
+    </Provider>
   );
 }
 
